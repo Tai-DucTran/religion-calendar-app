4de5fac2557f41e6759b5fc15fb0c959d563009a
Tai-DucTran <taitran.deve@gmail.com>
2024-03-31 21:43:49 +0700

    Packages/aries: created icons implementation

--- packages/aries/lib/src/icons/icons.dart
+++ packages/aries/lib/src/icons/icons.dart
@@ -3,4 +3,5 @@ class AriesIcons {
   static const facebookIcon = 'assets/icons/facebook_icon.svg';
   static const googleIcon = 'assets/icons/google_icon.svg';
   static const appleIcon = 'assets/icons/apple_icon.svg';
+  static const accountBoxLine = 'assets/images/icons/account-box-line.svg';
 }
