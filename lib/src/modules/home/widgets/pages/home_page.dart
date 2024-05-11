02bbf6916f6da86d894f1072ddfaf096562b17b7
Tai-DucTran <taitran.deve@gmail.com>
2024-05-11 21:04:09 +0700

    Home: created home_page

---
+++ lib/src/modules/home/widgets/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:full_calender/enums/time_zone.dart';
+import 'package:full_calender/full_calender.dart';
+import 'package:full_calender/full_calender_extension.dart';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HomePage extends HookConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final lunarDate =
+        FullCalender.now(TimeZone.indonesiaUTC8.timezone).lunarDate;
+    final solarDate =
+        FullCalenderExtension.convertLunarDateToSolarDate(lunarDate);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Home',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('This is solar cal $solarDate'),
+            Text('This is lunar cal $lunarDate'),
+          ],
+        ),
+      ),
+    );
+  }
+}
