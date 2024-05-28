e3ddfa1f157c43a2a89b7c5fca9b801b1cecb7a8
Tai-DucTran <taitran.deve@gmail.com>
2024-05-28 21:00:53 +0700

    Nit: modified export widgets.dart

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,4 +1,1 @@
-export 'bottom_navigation_bar/bottom_navigation_bar.dart';
-export 'bottom_navigation_bar/bottom_nav_bar.dart';
-export 'bottom_navigation_bar/navbar_tab.dart';
 export 'custom_elevated_button.dart';
