10c8ce3d12f4d3b5cf11bec05f29d87ead54ed3d
Tai-DucTran <taitran.deve@gmail.com>
2024-06-09 16:32:02 +0700

    aries: built images library

--- packages/aries/lib/aries.dart
+++ packages/aries/lib/aries.dart
@@ -1,3 +1,4 @@
 export 'src/spacing/spacing.dart';
 export 'src/icons/icons.dart';
+export 'src/images/images.dart';
 export 'src/colors/colors.dart';
---
+++ packages/aries/lib/src/images/images.dart
@@ -0,0 +1,6 @@
+class Images {
+  Images._();
+
+  static const splashScreenImage =
+      'packages/aries/assets/images/others/welcome_page.svg';
+}
