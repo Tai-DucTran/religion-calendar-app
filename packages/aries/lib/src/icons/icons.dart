98473e8e705874560e96c1eb166ad834ca4db70f
Tai-DucTran <taitran.deve@gmail.com>
2024-06-11 23:46:27 +0700

    aries: added new social icons

--- packages/aries/lib/src/icons/icons.dart
+++ packages/aries/lib/src/icons/icons.dart
@@ -2,9 +2,14 @@ class AriesIcons {
   AriesIcons._();
   static const facebookIcon =
       'packages/aries/assets/images/icons/facebook_icon.svg';
+  static const facebookWhiteIcon =
+      'packages/aries/assets/images/icons/facebook_icon_white_background.svg';
   static const googleIcon =
       'packages/aries/assets/images/icons/google_icon.svg';
-  static const appleIcon = 'packages/aries/assets/images/icons/apple_icon.svg';
+  static const appleBlackIcon =
+      'packages/aries/assets/images/icons/apple_icon.svg';
+  static const appleWhiteIcon =
+      'packages/aries/assets/images/icons/apple_icon_white_background.svg';
   static const accountBoxLine =
       'packages/aries/assets/images/icons/account-box-line.svg';
 }
