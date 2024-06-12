2c0c9557f11f45fa219ae964cfa94f9de18719e1
Tai-DucTran <taitran.deve@gmail.com>
2024-06-12 23:36:20 +0700

    widgets: refactored custom_elevated_button ui

--- lib/src/widgets/custom_elevated_button.dart
+++ lib/src/widgets/custom_elevated_button.dart
@@ -1,30 +1,41 @@
+import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
 
 class CustomElevatedButton extends StatelessWidget {
-  final double marginHorizontal;
-  final double marginVertical;
-  final Color? buttonColor;
-  final double radius;
-  final String text;
-  final Color textColor;
-  final double fontSize;
-  final VoidCallback onPressed;
-
   const CustomElevatedButton({
     super.key,
     this.marginHorizontal = 0,
     this.marginVertical = 0,
     this.fontSize = 18,
+    this.height = 48,
     this.radius = 8,
     required this.buttonColor,
     required this.text,
-    required this.textColor,
     required this.onPressed,
+    this.width,
+    this.textStyle = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: AriesColor.neutral900,
+    ),
   });
+
+  final double marginHorizontal;
+  final double marginVertical;
+  final Color? buttonColor;
+  final double radius;
+  final String text;
+  final double fontSize;
+  final VoidCallback onPressed;
+  final double? width;
+  final double? height;
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+        height: height,
+        width: width,
         margin: EdgeInsets.symmetric(
           vertical: marginVertical,
           horizontal: marginHorizontal,
@@ -41,10 +52,7 @@ class CustomElevatedButton extends StatelessWidget {
           ),
           child: Text(
             text,
-            style: TextStyle(
-              color: textColor,
-              fontSize: fontSize,
-            ),
+            style: textStyle,
           ),
         ));
   }
