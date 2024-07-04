a93b441aa1e79642ff3ec947debe48ec2cb30ad1
Tai-DucTran <taitran.deve@gmail.com>
2024-07-04 20:48:46 +0700

    firebase: refactored firebase_collection_name, firebase_field_name

--- lib/src/constants/firebase_collection_name.dart
+++ lib/src/constants/firebase_collection_name.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/foundation.dart' show immutable;
-
-@immutable
 class FirebaseCollectionName {
   static const thumbnails = 'thumbnails';
   static const users = 'users';
   static const userEvents = 'userEvents';
+  static const religions = 'religions';
   const FirebaseCollectionName._();
 }
