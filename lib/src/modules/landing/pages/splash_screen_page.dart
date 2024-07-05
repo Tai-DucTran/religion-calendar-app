957eea263bf73bb2066a0c465a8e2560f6c5d084
Tai-DucTran <taitran.deve@gmail.com>
2024-07-05 18:41:29 +0700

    SignUp: enable splash_screen_page when initializing the app https://glints.atlassian.net/browse/VN-1069

--- lib/src/modules/landing/pages/splash_screen_page.dart
+++ lib/src/modules/landing/pages/splash_screen_page.dart
@@ -1,41 +1,9 @@
 import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:religion_calendar_app/src/modules/sign_up/sign_up.dart';
 
-class SplashScreenPage extends StatefulWidget {
+class SplashScreenPage extends StatelessWidget {
   const SplashScreenPage({super.key});
-
-  @override
-  State<SplashScreenPage> createState() => _SplashScreenPageState();
-}
-
-class _SplashScreenPageState extends State<SplashScreenPage>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
-    Future.delayed(
-      const Duration(seconds: 2),
-      () {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (_) => const SignUpPage(),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: SystemUiOverlay.values);
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
