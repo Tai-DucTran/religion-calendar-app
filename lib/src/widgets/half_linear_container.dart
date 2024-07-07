eef5eb20d9dd1a03e778fa493ca9e5a15a844293
Tai-DucTran <taitran.deve@gmail.com>
2024-07-07 14:24:29 +0700

    Widgets: refactored half_linear_container

--- lib/src/widgets/half_linear_container.dart
+++ lib/src/widgets/half_linear_container.dart
@@ -1,50 +1,80 @@
 import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:religion_calendar_app/src/widgets/models/linear_first_section_ratio.dart';
 import 'package:religion_calendar_app/src/widgets/widgets.dart';
 
 class HalfLinearContainer extends StatelessWidget {
   const HalfLinearContainer({
     super.key,
     required this.child,
+    this.firstSectionRatio,
+    this.imagePath,
   });
 
   final Widget child;
+  final LinearFirstSectionRatio? firstSectionRatio;
+  final String? imagePath;
 
   @override
   Widget build(BuildContext context) {
+    final isHasImageInput = imagePath != null;
+    final ratio =
+        firstSectionRatio?.ratio ?? LinearFirstSectionRatio.defaultSized.ratio;
+
     return Scaffold(
       body: GradientBackground(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(top: 54),
-              child: SvgPicture.asset(AriesImages.sunAndMoonImage),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: const Alignment(0.0, -0.95),
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        AriesColor.neutral0.withOpacity(0),
-                        AriesColor.neutral0,
-                      ],
-                      stops: const [
-                        0,
-                        0.1
-                      ]),
+        child: LayoutBuilder(
+          builder: (
+            context,
+            constraints,
+          ) {
+            final screenHeight = constraints.maxHeight;
+            final firstSectionHeight = screenHeight * ratio;
+            final secondSectionHeight = screenHeight - firstSectionHeight;
+            final beginAlignment = firstSectionRatio?.beginAlignment ??
+                LinearFirstSectionRatio.defaultSized.beginAlignment;
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: firstSectionHeight,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 60),
+                    child: isHasImageInput
+                        ? Image.asset(imagePath!)
+                        : SvgPicture.asset(
+                            AriesIcons.sunAndMoonImageIcon,
+                          ),
+                  ),
                 ),
-                child: child,
-              ),
-            ),
-          ],
+                SizedBox(
+                  height: secondSectionHeight,
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 24,
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: beginAlignment,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          AriesColor.neutral0.withOpacity(0),
+                          AriesColor.neutral0,
+                        ],
+                        stops: const [0, 0.1],
+                      ),
+                    ),
+                    child: child,
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
---
+++ lib/src/widgets/models/linear_first_section_ratio.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LinearFirstSectionRatio {
+  const LinearFirstSectionRatio._(this.ratio, this.alignment);
+
+  final double ratio;
+  final Alignment alignment;
+
+  static const defaultSized = LinearFirstSectionRatio._(
+    3.5 / 10,
+    Alignment(0.0, -0.95),
+  );
+  static const halfSized = LinearFirstSectionRatio._(
+    1 / 2,
+    Alignment(0.0, -0.95),
+  );
+  static const bigSized = LinearFirstSectionRatio._(
+    3 / 5,
+    Alignment(0.0, -0.85),
+  );
+
+  Alignment get beginAlignment => alignment;
+}
