98b3f91e405c4a5a6ea82e13f905675952ec372e
Tai-DucTran <taitran.deve@gmail.com>
2024-07-28 15:03:04 +0700

    Auth: added new sign-in method createUserWithEmailAndPassword loginWithEmailAndPassword https://taisidehustle.atlassian.net/browse/KAN-10

--- lib/src/modules/authentication/repositories/authenticator_repo.dart
+++ lib/src/modules/authentication/repositories/authenticator_repo.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:religion_calendar_app/src/modules/authentication/exceptions/authenticator_exceptions.dart';
 import 'package:religion_calendar_app/src/modules/authentication/models/auth_results.dart';
 import 'package:religion_calendar_app/src/modules/authentication/constants/auth_constants.dart';
 import 'package:religion_calendar_app/src/modules/user/models/user_id.dart';
@@ -23,8 +24,10 @@ class AuthenticatorRepository {
   String get displayName => currentUser?.displayName ?? '';
   String? get email => currentUser?.email;
 
+  FirebaseAuth get _auth => FirebaseAuth.instance;
+
   Future<void> logOut() async {
-    await FirebaseAuth.instance.signOut();
+    await _auth.signOut();
     await GoogleSignIn().signOut();
     await FacebookAuth.instance.logOut();
   }
@@ -42,7 +45,7 @@ class AuthenticatorRepository {
     final oauthCredentials = FacebookAuthProvider.credential(token);
 
     try {
-      await FirebaseAuth.instance.signInWithCredential(
+      await _auth.signInWithCredential(
         oauthCredentials,
       );
       return AuthResults.success;
@@ -73,7 +76,7 @@ class AuthenticatorRepository {
     );
 
     try {
-      await FirebaseAuth.instance.signInWithCredential(
+      await _auth.signInWithCredential(
         oauthCredentials,
       );
       return AuthResults.success;
@@ -82,6 +85,65 @@ class AuthenticatorRepository {
         code: e.code,
         message: e.message,
       );
+    }
+  }
+
+  Future<AuthResults> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final user = currentUser;
+      if (user != null) {
+        return AuthResults.success;
+      } else {
+        throw UserNotLoggedInAuthException();
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw WeakPasswordAuthException();
+      } else if (e.code == 'email-already-in-use') {
+        throw EmailAlreadyInUseAuthException();
+      } else if (e.code == 'invalid-email') {
+        throw InvalidEmailAuthException();
+      } else {
+        throw GenericAuthException();
+      }
+    } catch (_) {
+      throw GenericAuthException();
+    }
+  }
+
+  Future<AuthResults> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = currentUser;
+      if (user != null) {
+        return AuthResults.success;
+      } else {
+        throw UserNotLoggedInAuthException();
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        throw UserNotFoundAuthException();
+      } else if (e.code == 'wrong-password') {
+        throw WrongPasswordAuthException();
+      } else {
+        throw GenericAuthException();
+      }
+    } catch (_) {
+      throw GenericAuthException();
     }
   }
 }
