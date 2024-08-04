108c40ed77d835d20681292319f8b56978c54313
Tai-DucTran <taitran.deve@gmail.com>
2024-08-04 14:55:15 +0700

    Login: handle error states

--- lib/src/modules/login/widgets/organisms/login_form_field.dart
+++ lib/src/modules/login/widgets/organisms/login_form_field.dart
@@ -1,6 +1,7 @@
 import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:religion_calendar_app/src/modules/authentication/exceptions/authenticator_exceptions.dart';
 import 'package:religion_calendar_app/src/widgets/widgets.dart';
 
 import '../../../authentication/authentication.dart';
@@ -18,12 +19,40 @@ class _LoginFormFieldsState extends ConsumerState<LoginFormFields> {
   final TextEditingController _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool _isEmailValid = true;
+  OverlayEntry? overlayEntry;
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
+    cleanOverlayEntry();
     super.dispose();
+  }
+
+  void cleanOverlayEntry() {
+    overlayEntry?.remove();
+    overlayEntry?.dispose();
+    overlayEntry = null;
+  }
+
+  void _showErrorOverlay({
+    required String title,
+    required String message,
+    required bool isErrorOverlay,
+    IconData? icon,
+  }) {
+    cleanOverlayEntry();
+
+    overlayEntry = OverlayEntry(
+      builder: (context) => ErrorOverlayTopModal(
+        errorTitle: title,
+        errorMessage: message,
+        onClose: cleanOverlayEntry,
+        isErrorOverlay: isErrorOverlay,
+        icon: icon,
+      ),
+    );
+    Overlay.of(context).insert(overlayEntry!);
   }
 
   @override
@@ -53,10 +82,40 @@ class _LoginFormFieldsState extends ConsumerState<LoginFormFields> {
                 });
                 final authController =
                     ref.read(authStateControllerProvider.notifier);
-                await authController.loginWithEmailAndPassword(
-                  email: _emailController.text,
-                  password: _passwordController.text,
-                );
+                try {
+                  await authController.loginWithEmailAndPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                  );
+                } catch (error) {
+                  if (!mounted) return;
+                  if (error is InvalidCredentialAuthException) {
+                    _showErrorOverlay(
+                      title: 'Invalid Credential',
+                      message:
+                          'Your email or password is incorrect, please try again',
+                      isErrorOverlay: true,
+                    );
+                  } else if (error is WrongPasswordAuthException) {
+                    _showErrorOverlay(
+                      title: 'Wrong Password',
+                      message: 'Your password is incorrect, please try again',
+                      isErrorOverlay: true,
+                    );
+                  } else if (error is UserNotFoundAuthException) {
+                    _showErrorOverlay(
+                      title: 'Account Not Found',
+                      message: 'We cannot find your account, please try again!',
+                      isErrorOverlay: true,
+                    );
+                  } else {
+                    _showErrorOverlay(
+                      title: 'Error: $error',
+                      message: 'An unexpected error occurred.',
+                      isErrorOverlay: true,
+                    );
+                  }
+                }
               },
             ),
           ],
