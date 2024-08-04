2fbd4250931fb84782b59abde816ce8b959c4422
Tai-DucTran <taitran.deve@gmail.com>
2024-08-04 14:15:13 +0700

    Housekeeping: added border for error state in error_overlay_top_modal

--- lib/src/widgets/top_status_bar/error_overlay_top_modal.dart
+++ lib/src/widgets/top_status_bar/error_overlay_top_modal.dart
@@ -9,12 +9,14 @@ class ErrorOverlayTopModal extends StatelessWidget {
     required this.errorMessage,
     this.onClose,
     required this.isErrorOverlay,
+    this.icon,
   });
 
   final String errorTitle;
   final String errorMessage;
   final VoidCallback? onClose;
   final bool isErrorOverlay;
+  final IconData? icon;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +33,12 @@ class ErrorOverlayTopModal extends StatelessWidget {
             borderRadius: BorderRadius.circular(
               12,
             ),
+            border: isErrorOverlay
+                ? Border.all(
+                    color: AriesColor.danger75,
+                    width: 1,
+                  )
+                : null,
             boxShadow: [
               BoxShadow(
                 color: isErrorOverlay
@@ -43,72 +51,68 @@ class ErrorOverlayTopModal extends StatelessWidget {
               ),
             ],
           ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              isErrorOverlay
-                  ? SvgPicture.asset(
-                      AriesIcons.alertCircleOutlineIcon,
-                      height: 20,
-                      width: 20,
-                    )
-                  : Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x1A101828),
-                            offset: Offset(0, 1),
-                            blurRadius: 2,
-                            spreadRadius: 0,
+          child: ListTile(
+            contentPadding: EdgeInsetsDirectional.zero,
+            dense: true,
+            isThreeLine: true,
+            minLeadingWidth: 12,
+            minVerticalPadding: 0,
+            leading: isErrorOverlay
+                ? SvgPicture.asset(
+                    AriesIcons.alertCircleOutlineIcon,
+                    height: 20,
+                    width: 20,
+                  )
+                : Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x1A101828),
+                          offset: Offset(0, 1),
+                          blurRadius: 2,
+                          spreadRadius: 0,
+                        ),
+                      ],
+                    ),
+                    child: (icon != null)
+                        ? Icon(
+                            icon,
+                            size: 20,
+                          )
+                        : SvgPicture.asset(
+                            AriesIcons.googleIcon,
+                            height: 20,
+                            width: 20,
                           ),
-                        ],
-                      ),
-                      child: SvgPicture.asset(
-                        AriesIcons.googleIcon,
-                        height: 20,
-                        width: 20,
-                      ),
-                    ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      errorTitle,
-                      style: TextStyle(
-                        color: isErrorOverlay
-                            ? AriesColor.danger300
-                            : AriesColor.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      errorMessage,
-                      style: TextStyle(
-                        color: isErrorOverlay
-                            ? AriesColor.danger300
-                            : AriesColor.black,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+            title: Text(
+              errorTitle,
+              style: TextStyle(
+                color: isErrorOverlay ? AriesColor.danger300 : AriesColor.black,
+                fontWeight: FontWeight.bold,
               ),
-              IconButton(
+            ),
+            subtitle: Text(
+              errorMessage,
+              style: TextStyle(
+                color: isErrorOverlay ? AriesColor.danger300 : AriesColor.black,
+              ),
+            ),
+            trailing: GestureDetector(
+              onTap: onClose,
+              child: Padding(
                 padding: EdgeInsets.zero,
-                alignment: Alignment.topRight,
-                icon: Icon(
+                child: Icon(
                   Icons.close,
                   color: isErrorOverlay
                       ? AriesColor.danger200
                       : AriesColor.neutral300,
+                  size: 20,
                 ),
-                onPressed: onClose,
               ),
-            ],
+            ),
           ),
         ),
       ),
