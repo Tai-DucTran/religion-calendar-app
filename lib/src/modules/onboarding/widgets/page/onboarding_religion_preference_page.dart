8c55677cc40438325859470bba43c0af4f25ae17
Tai-DucTran <taitran.deve@gmail.com>
2024-08-06 21:04:37 +0700

    Onboarding: implemented onboarding flow

--- lib/src/modules/onboarding/widgets/page/onboarding_religion_preference_page.dart
+++ lib/src/modules/onboarding/widgets/page/onboarding_religion_preference_page.dart
@@ -1,17 +1,28 @@
 import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:religion_calendar_app/src/helper/string_helper.dart';
+import 'package:religion_calendar_app/src/modules/authentication/authentication.dart';
 import 'package:religion_calendar_app/src/modules/onboarding/controllers/get_religion_onboarding_image_controller.dart';
+import 'package:religion_calendar_app/src/modules/router/routes.dart';
 import 'package:religion_calendar_app/src/modules/user/models/models.dart';
+import 'package:religion_calendar_app/src/modules/user/repositories/user_firestore_repo.dart';
+import 'package:religion_calendar_app/src/utils/log.dart';
 import 'package:religion_calendar_app/src/widgets/widgets.dart';
 
-class OnboardingPage extends ConsumerWidget {
-  const OnboardingPage({super.key});
+class OnboardingReligionPreferencePage extends ConsumerWidget {
+  const OnboardingReligionPreferencePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final imagePath = ref.watch(selectedReligionProvider);
+    final currentReligionPref =
+        ref.watch(currentReligionProvider.notifier).state.name.toUpperCase();
+    final userId = ref.watch(authenticatorRepositoryProvider).currentUser?.uid;
+
+    currentReligionPref.log();
+    userId?.log();
 
     return HalfLinearContainer(
       imagePath: imagePath,
@@ -72,8 +83,23 @@ class OnboardingPage extends ConsumerWidget {
                 );
               },
             ),
-            CtaFullWidthButton(buttonText: 'Select', onPressed: () {}),
+            CtaFullWidthButton(
+              buttonText: 'Select',
+              onPressed: () async {
+                final userFirestoreController =
+                    ref.read(userFirestoreRepositoryProvider);
+                userFirestoreController.updateReligionPreferenceOnboarding(
+                  userId: userId,
+                  religionPreference: currentReligionPref,
+                );
+                ref
+                    .read(authStateControllerProvider.notifier)
+                    .updateOnboardingStatus(true);
+                context.go(const HomeRoute().location);
+              },
+            ),
             // TODO (Tai): Implement submit request form
+            Spacing.sp24,
             BottomRichTextWithAction(
               initialQuestion: 'You want to have your religion? ',
               textSpan: 'Submit here',
