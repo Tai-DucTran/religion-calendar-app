06237e8d838b6e37b359c0aa1380a5ec8ec8036e
Tai-DucTran <taitran.deve@gmail.com>
2024-08-06 21:02:10 +0700

    Housekeeing: added more values in firebase_field_name hasCompleteOnboarding isVerified

--- lib/src/constants/firebase_field_name.dart
+++ lib/src/constants/firebase_field_name.dart
@@ -2,12 +2,17 @@ import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
 class FirebaseFieldName {
+  /// User properties
   static const userId = 'userId';
-  static const email = 'email';
   static const displayName = 'displayName';
+  static const email = 'email';
   static const createdAt = 'createdAt';
   static const updatedAt = 'updatedAt';
-  static const religionReference = 'religionReference';
+  static const hasCompleteOnboarding = 'hasCompleteOnboarding';
+  static const isVerified = 'isVerified';
+  static const religionPreference = 'religionPreference';
+
+  /// Event properties
   static const eventId = 'eventId';
   static const eventName = 'eventName';
   static const eventDescription = 'eventDescription';
