745a4bee030eefc47080fdaafe4b011131e89bd6
Tai-DucTran <taitran.deve@gmail.com>
2024-08-17 23:37:40 +0700

    Home: handle clickable animation on date_section

--- lib/src/modules/home/widgets/molecules/date_section.dart
+++ lib/src/modules/home/widgets/molecules/date_section.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:religion_calendar_app/src/modules/calendar/calendar.dart';
+import 'package:religion_calendar_app/src/utils/log.dart';
 
 class DateSection extends ConsumerWidget {
   const DateSection({
@@ -22,60 +23,77 @@ class DateSection extends ConsumerWidget {
     final String date = DateFormat('d').format(inputDate);
     final lunarDate = getLunarDate(date: inputDate);
 
-    return Column(
-      children: [
-        Text(
-          weekdayName,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                color:
-                    isNotInCurrentMonth ? Colors.grey : AriesColor.neutral400,
-                fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
-              ),
+    return Material(
+      color: Colors.transparent,
+      borderRadius: BorderRadius.circular(4),
+      child: InkWell(
+        onTap: () {
+          'selecting date card'.log();
+        },
+        borderRadius: BorderRadius.circular(4),
+        splashColor: AriesColor.yellowP200.withOpacity(
+          0.3,
         ),
-        Container(
-          padding: const EdgeInsets.only(
-            top: 4,
-            left: 12,
-            right: 12,
-            bottom: 4,
-          ),
-          margin: const EdgeInsets.all(4.0),
-          decoration: BoxDecoration(
-            color: isToday ? AriesColor.yellowP50 : Colors.transparent,
-            borderRadius: BorderRadius.circular(4),
-            border: isToday
-                ? Border.all(
-                    color: AriesColor.yellowP50,
-                    width: 0,
-                  )
-                : null,
-          ),
-          child: Column(
-            children: [
-              Text(
-                date,
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: isNotInCurrentMonth ? Colors.grey : Colors.black,
-                      fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
-                    ),
+        child: Column(
+          children: [
+            Text(
+              weekdayName,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: isNotInCurrentMonth
+                        ? Colors.grey
+                        : AriesColor.neutral400,
+                    fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
+                  ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 4,
+                left: 12,
+                right: 12,
+                bottom: 4,
               ),
-              Text(
-                textAlign: TextAlign.right,
-                lunarDate,
-                style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                      color: isNotInCurrentMonth
-                          ? Colors.grey
-                          : isToday
-                              ? AriesColor.yellowP500
-                              : Colors.black,
-                      fontSize: 10,
-                      fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
-                    ),
+              margin: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                color: isToday ? AriesColor.yellowP50 : Colors.transparent,
+                borderRadius: BorderRadius.circular(4),
+                border: isToday
+                    ? Border.all(
+                        color: AriesColor.yellowP50,
+                        width: 0,
+                      )
+                    : null,
               ),
-            ],
-          ),
+              child: Column(
+                children: [
+                  Text(
+                    date,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color:
+                              isNotInCurrentMonth ? Colors.grey : Colors.black,
+                          fontWeight:
+                              isToday ? FontWeight.bold : FontWeight.normal,
+                        ),
+                  ),
+                  Text(
+                    textAlign: TextAlign.right,
+                    lunarDate,
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: isNotInCurrentMonth
+                              ? Colors.grey
+                              : isToday
+                                  ? AriesColor.yellowP500
+                                  : Colors.black,
+                          fontSize: 10,
+                          fontWeight:
+                              isToday ? FontWeight.bold : FontWeight.normal,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
