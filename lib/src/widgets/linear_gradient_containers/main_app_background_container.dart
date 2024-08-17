273ad8964592c4d228626fae134d520ffb99e155
Tai-DucTran <taitran.deve@gmail.com>
2024-08-17 23:00:39 +0700

    Widgets: refactored main_app_background_container Disable/Enable scrollable based on the height of the screen

--- lib/src/widgets/linear_gradient_containers/main_app_background_container.dart
+++ lib/src/widgets/linear_gradient_containers/main_app_background_container.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 
-class MainAppBackgroundContainer extends StatelessWidget {
+class MainAppBackgroundContainer extends StatefulWidget {
+  final Widget child;
+
   const MainAppBackgroundContainer({super.key, required this.child});
 
-  final Widget child;
+  @override
+  State<MainAppBackgroundContainer> createState() =>
+      _MainAppBackgroundContainerState();
+}
+
+class _MainAppBackgroundContainerState
+    extends State<MainAppBackgroundContainer> {
+  final GlobalKey _childKey = GlobalKey();
+  bool _isScrollable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkIfScrollable();
+    });
+  }
+
+  void _checkIfScrollable() {
+    final RenderBox renderBox =
+        _childKey.currentContext?.findRenderObject() as RenderBox;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final childHeight = renderBox.size.height;
+
+    setState(() {
+      _isScrollable = childHeight > screenHeight;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: double.infinity,
+      padding: const EdgeInsets.all(16),
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
@@ -20,6 +50,25 @@ class MainAppBackgroundContainer extends StatelessWidget {
             Color(0xFFF1E6C7), // #F1E6C7
           ],
           stops: [0.0, 0.695, 1.0],
+        ),
+      ),
+      child: SafeArea(
+        child: CustomScrollView(
+          physics: _isScrollable
+              ? const ClampingScrollPhysics()
+              : const NeverScrollableScrollPhysics(),
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Container(
+                    key: _childKey,
+                    child: widget.child,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
