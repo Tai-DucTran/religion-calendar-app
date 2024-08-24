f2b83a91d566c1da61d197e3f17cfb75d579859d
Tai-DucTran <taitran.deve@gmail.com>
2024-08-24 14:16:44 +0700

    HomePage: refactored UI

--- lib/src/modules/home/widgets/atoms/atoms.dart
+++ lib/src/modules/home/widgets/atoms/atoms.dart
@@ -1,1 +1,2 @@
 export 'dash_divider.dart';
+export 'upcoming_events_header.dart';
--- lib/src/modules/home/widgets/organisms/organisms.dart
+++ lib/src/modules/home/widgets/organisms/organisms.dart
@@ -1,2 +1,3 @@
 export 'current_date_card.dart';
+export 'upcoming_events_section.dart';
 export 'weekly_calendar_section.dart';
