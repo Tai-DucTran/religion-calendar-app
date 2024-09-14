8ec675ce79f5af2f1a8c57e55b3df3a11d49a3d3
Tai-DucTran <taitran.deve@gmail.com>
2024-09-14 23:29:40 +0700

    User: created extension for religion_prefernce

--- lib/src/modules/user/models/religion_prefernce.dart
+++ lib/src/modules/user/models/religion_prefernce.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:religion_calendar_app/l10n/localized_keys.dart';
 
 enum ReligionPreference {
   @JsonValue('CATHOLICISM')
@@ -7,4 +8,18 @@ enum ReligionPreference {
   buddhism,
   @JsonValue('UNKNOWN')
   unknown,
+}
+
+extension ReligionPrefernceExtension on ReligionPreference {
+  String get localized {
+    switch (this) {
+      case ReligionPreference.catholicism:
+        return LocalizedKeys.religionCatholicismText;
+      case ReligionPreference.buddhism:
+        return LocalizedKeys.religionBuddhismText;
+      case ReligionPreference.unknown:
+      default:
+        return LocalizedKeys.religionUnknownText;
+    }
+  }
 }
