7176c3e15f25aea4bff5cdf661dcebcaf8bf6784
Tai-DucTran <taitran.deve@gmail.com>
2024-09-27 19:47:30 +0700

    Calendar: fixed ImpellerValidationBreak issue

--- lib/src/modules/calendar/widgets/molecules/date_time_picker_section.dart
+++ lib/src/modules/calendar/widgets/molecules/date_time_picker_section.dart
@@ -6,6 +6,7 @@ import 'package:intl/intl.dart';
 import 'package:religion_calendar_app/l10n/localized_keys.dart';
 import 'package:religion_calendar_app/src/constants/constants.dart';
 import 'package:religion_calendar_app/src/modules/calendar/calendar.dart';
+import 'dart:async';
 
 class DateTimePickerSection extends ConsumerStatefulWidget {
   const DateTimePickerSection({
@@ -23,10 +24,38 @@ class DateTimePickerSection extends ConsumerStatefulWidget {
 }
 
 class _DateTimePickerSectionState extends ConsumerState<DateTimePickerSection> {
+  /// [_debounce], [_pendingUpdate], [_isScrolling], [_scrollSettleTime]
+  /// These values helps to resolve the issue
+  ///! ERROR:flutter/impeller/base/validation.cc(59)] Break on 'ImpellerValidationBreak'
+  ///! to inspect point of failure: Could not find font in the atlas.
+  ///
+  /// This happens because the state of rendered time update so frequently along with
+  /// the frequency of updates and the likelihood of race conditions of dateTime
+  ///
+  ///* Solution:
+  /// - introduces a delay before applying updates, which can help prevent rapid.
+  /// - before updating the dateTime value on screen, it need to wait [_scrollSettleTime]
+  ///  to ensure the scrolling behavior in [CupertinoDatePicker] stops completely
+  ///
+  /// TODO (Tai): Refactor this widget, self implementation instead of using [CupertinoDatePicker]
+  /// Consider to refactor if the app reach over 10k users or we want to support different calendar
+  /// categories such as Islamic lunar calendar using [hijri] library
+  /// source: https://pub.dev/packages/hijri
+  Timer? _debounce;
+  DateTime? _pendingUpdate;
+  bool _isScrolling = false;
+  static const _scrollSettleTime = Duration(milliseconds: 200);
+
   @override
   void initState() {
     super.initState();
     _initializeDate();
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
   }
 
   void _initializeDate() {
@@ -54,6 +83,55 @@ class _DateTimePickerSectionState extends ConsumerState<DateTimePickerSection> {
     }
   }
 
+  void _onScrollStart() {
+    if (!_isScrolling) {
+      setState(() {
+        _isScrolling = true;
+      });
+    }
+  }
+
+  void _onScrollEnd(DateTime newDateTime, bool isDate) {
+    _pendingUpdate = newDateTime;
+    _debounce?.cancel();
+    _debounce = Timer(_scrollSettleTime, () {
+      if (mounted) {
+        setState(() {
+          _isScrolling = false;
+        });
+        _updateDateTime(isDate);
+      }
+      _updateDateTime(isDate);
+    });
+  }
+
+  void _updateDateTime(bool isDate) {
+    if (_pendingUpdate != null) {
+      if (widget.isStartDate) {
+        if (isDate) {
+          ref
+              .read(eventDateTimeControllerProvider.notifier)
+              .setStartDate(_pendingUpdate!);
+        } else {
+          ref
+              .read(eventDateTimeControllerProvider.notifier)
+              .setStartTime(_pendingUpdate!);
+        }
+      } else {
+        if (isDate) {
+          ref
+              .read(eventDateTimeControllerProvider.notifier)
+              .setEndDate(_pendingUpdate!);
+        } else {
+          ref
+              .read(eventDateTimeControllerProvider.notifier)
+              .setEndTime(_pendingUpdate!);
+        }
+      }
+      _pendingUpdate = null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final eventDateTime = ref.watch(eventDateTimeControllerProvider);
@@ -69,47 +147,57 @@ class _DateTimePickerSectionState extends ConsumerState<DateTimePickerSection> {
       isThreeLine: true,
       dense: true,
       contentPadding: EdgeInsets.zero,
-      leading: Padding(
-        padding: const EdgeInsets.only(top: 8),
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 4),
-          width: 85,
-          height: 30,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(6),
-            border: Border.all(color: AriesColor.neutral50),
-          ),
-          child: Text(
-            widget.isStartDate
-                ? LocalizedKeys.startingEventDateText
-                : LocalizedKeys.endingEventDateText,
-          ),
-        ),
-      ),
-      title: CupertinoButton(
-        padding: EdgeInsets.zero,
-        alignment: Alignment.centerLeft,
-        onPressed: () => _showDatePicker(context, date),
-        child: Text(
-          DateFormat(DateTimeFormat.dateShortMonthYear, currentLocale)
-              .format(date),
-        ),
-      ),
+      leading: _buildLeadingLabel(),
+      title: _buildDateButton(context, date, currentLocale),
       subtitle: DateSubtitlePicker(
         locale: currentLocale,
         selectedDate: date,
       ),
       trailing: !isAllDay
-          ? CupertinoButton(
-              padding: const EdgeInsets.only(bottom: 0),
-              onPressed: () => _showTimePicker(context, time),
-              child: Text(
-                DateFormat(DateTimeFormat.hourMinute, currentLocale)
-                    .format(time!),
-              ),
-            )
+          ? _buildTimeButton(context, time!, currentLocale)
           : const Offstage(),
+    );
+  }
+
+  Widget _buildLeadingLabel() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        width: 85,
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          border: Border.all(color: AriesColor.neutral50),
+        ),
+        child: Text(
+          widget.isStartDate
+              ? LocalizedKeys.startingEventDateText
+              : LocalizedKeys.endingEventDateText,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDateButton(BuildContext context, DateTime date, String locale) {
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      alignment: Alignment.centerLeft,
+      onPressed: () => _showDatePicker(context, date),
+      child: Text(
+        DateFormat(DateTimeFormat.dateShortMonthYear, locale).format(date),
+      ),
+    );
+  }
+
+  Widget _buildTimeButton(BuildContext context, DateTime time, String locale) {
+    return CupertinoButton(
+      padding: const EdgeInsets.only(bottom: 0),
+      onPressed: () => _showTimePicker(context, time),
+      child: Text(
+        DateFormat(DateTimeFormat.hourMinute, locale).format(time),
+      ),
     );
   }
 
@@ -120,20 +208,22 @@ class _DateTimePickerSectionState extends ConsumerState<DateTimePickerSection> {
         return Container(
           height: 250,
           color: AriesColor.neutral0,
-          child: CupertinoDatePicker(
-            mode: CupertinoDatePickerMode.date,
-            initialDateTime: initialDate,
-            onDateTimeChanged: (DateTime newDate) {
-              if (widget.isStartDate) {
-                ref
-                    .read(eventDateTimeControllerProvider.notifier)
-                    .setStartDate(newDate);
-              } else {
-                ref
-                    .read(eventDateTimeControllerProvider.notifier)
-                    .setEndDate(newDate);
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              if (notification is ScrollStartNotification) {
+                _onScrollStart();
+              } else if (notification is ScrollEndNotification) {
+                _onScrollEnd(initialDate, true);
               }
+              return true;
             },
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: initialDate,
+              onDateTimeChanged: (DateTime newDate) {
+                initialDate = newDate;
+              },
+            ),
           ),
         );
       },
@@ -147,21 +237,23 @@ class _DateTimePickerSectionState extends ConsumerState<DateTimePickerSection> {
         return Container(
           height: 250,
           color: AriesColor.neutral0,
-          child: CupertinoDatePicker(
-            mode: CupertinoDatePickerMode.time,
-            use24hFormat: true,
-            initialDateTime: initialTime,
-            onDateTimeChanged: (DateTime newTime) {
-              if (widget.isStartDate) {
-                ref
-                    .read(eventDateTimeControllerProvider.notifier)
-                    .setStartTime(newTime);
-              } else {
-                ref
-                    .read(eventDateTimeControllerProvider.notifier)
-                    .setEndTime(newTime);
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              if (notification is ScrollStartNotification) {
+                _onScrollStart();
+              } else if (notification is ScrollEndNotification) {
+                _onScrollEnd(initialTime, false);
               }
+              return true;
             },
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.time,
+              use24hFormat: true,
+              initialDateTime: initialTime,
+              onDateTimeChanged: (DateTime newTime) {
+                initialTime = newTime;
+              },
+            ),
           ),
         );
       },
