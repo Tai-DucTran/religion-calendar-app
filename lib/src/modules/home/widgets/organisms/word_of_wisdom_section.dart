ade1e1eb788fd3db5ed37fb3847ace93ff5e2e6c
Tai-DucTran <taitran.deve@gmail.com>
2024-10-16 21:24:36 +0700

    Home: clean up unused code

--- lib/src/modules/home/widgets/organisms/word_of_wisdom_section.dart
+++ lib/src/modules/home/widgets/organisms/word_of_wisdom_section.dart
@@ -13,7 +13,6 @@ class WordOfWisdomSection extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: 8.w,
       ),
-
       decoration: BoxDecoration(
         color: AriesColor.neutral0,
         borderRadius: BorderRadius.circular(16),
@@ -66,59 +65,6 @@ class WordOfWisdomSection extends StatelessWidget {
           ),
         ],
       ),
-
-      // decoration: BoxDecoration(
-      //   color: AriesColor.black,
-      //   borderRadius: BorderRadius.circular(16),
-      // ),
-      // child: Stack(
-      //   alignment: Alignment.center,
-      //   children: [
-      //     Positioned(
-      //       top: -150.h,
-      //       child: Opacity(
-      //         opacity: 0.4,
-      //         child: SvgPicture.asset(
-      //           AriesIcons.wordOfWisdomOutlineIcon,
-      //           fit: BoxFit.cover,
-      //         ),
-      //       ),
-      //     ),
-      //     Column(
-      //       crossAxisAlignment: CrossAxisAlignment.center,
-      //       children: [
-      //         Spacing.sp32,
-      //         Text(
-      //           LocalizedKeys.wordOfWisdom,
-      //           style: AriesTextStyles.textHeading6.copyWith(
-      //             color: AriesColor.yellowP200,
-      //             fontWeight: FontWeight.w700,
-      //           ),
-      //           textAlign: TextAlign.center,
-      //         ),
-      //         Spacing.sp16,
-      //         Text(
-      //           'Be kind, for everyone you meet is fighting a hard battle',
-      //           style: AriesTextStyles.textHeading5.copyWith(
-      //             color: AriesColor.neutral0,
-      //             fontSize: 18,
-      //           ),
-      //           textAlign: TextAlign.center,
-      //           maxLines: 5,
-      //         ),
-      //         Spacing.sp16,
-      //         Text(
-      //           'Socrates',
-      //           style: AriesTextStyles.textBodySmall.copyWith(
-      //             color: AriesColor.neutral0,
-      //           ),
-      //           textAlign: TextAlign.right,
-      //         ),
-      //         Spacing.sp20,
-      //       ],
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
