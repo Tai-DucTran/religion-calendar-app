bc774a9a24d87a1a049485dcac60616149667b83
Tai-DucTran <taitran.deve@gmail.com>
2024-10-24 08:51:28 +0700

    Calendar: created remind_me_before_select

--- lib/src/modules/calendar/widgets/molecules/molecules.dart
+++ lib/src/modules/calendar/widgets/molecules/molecules.dart
@@ -3,4 +3,5 @@ export 'event_category_select.dart';
 export 'event_location_input.dart';
 export 'is_day_toggle.dart';
 export 'is_lunar_calendar_toggle.dart';
+export 'remind_me_before_select.dart';
 export 'repeated_frequency_select.dart';
---
+++ lib/src/modules/calendar/widgets/molecules/remind_me_before_select.dart
@@ -0,0 +1,60 @@
+import 'package:aries/aries.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:religion_calendar_app/l10n/localized_keys.dart';
+import 'package:religion_calendar_app/src/modules/calendar/models/models.dart';
+
+class RemindMeBeforeSelect extends ConsumerWidget {
+  const RemindMeBeforeSelect({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final options = RemindMeBeforeOptions.values.toList();
+    const selectedOption = RemindMeBeforeOptions.atTime;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          LocalizedKeys.eventRemindMeBefore,
+          style: AriesTextStyles.textHeading6,
+        ),
+        Spacing.sp8,
+        IntrinsicWidth(
+          child: DropdownButton<RemindMeBeforeOptions>(
+            items: options.map(
+              (option) {
+                final isSelectedOption = option == selectedOption;
+                return DropdownMenuItem(
+                  value: option,
+                  child: Row(children: [
+                    Text(
+                      option.localized,
+                      style: AriesTextStyles.textHeading6.copyWith(
+                        color: isSelectedOption
+                            ? AriesColor.yellowP300
+                            : AriesColor.black,
+                        fontWeight: isSelectedOption
+                            ? FontWeight.bold
+                            : FontWeight.w400,
+                      ),
+                    ),
+                  ]),
+                );
+              },
+            ).toList(),
+            onChanged: (newValue) {},
+            value: selectedOption,
+            underline: const SizedBox.shrink(),
+            borderRadius: BorderRadius.circular(
+              8.r,
+            ),
+            dropdownColor: AriesColor.neutral0,
+            isExpanded: false,
+          ),
+        )
+      ],
+    );
+  }
+}
