9afd792bd5dd9b7665f21e05a7d882e19983a03d
Tai-DucTran <taitran.deve@gmail.com>
2024-10-24 08:52:30 +0700

    Calendar: created repeated_frequency_select

---
+++ lib/src/modules/calendar/widgets/molecules/repeated_frequency_select.dart
@@ -0,0 +1,54 @@
+import 'package:aries/aries.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:religion_calendar_app/src/modules/calendar/models/models.dart';
+
+class RepeatedFrequencySelect extends ConsumerWidget {
+  const RepeatedFrequencySelect({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final options = RepeatedFrequency.values.toList();
+    const selectedFrequency = RepeatedFrequency.doesNotRepeat;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Spacing.sp8,
+        IntrinsicWidth(
+          child: DropdownButton<RepeatedFrequency>(
+            items: options.map(
+              (option) {
+                final isSelectedOption = option == selectedFrequency;
+                return DropdownMenuItem(
+                  value: option,
+                  child: Row(children: [
+                    Text(
+                      option.localized,
+                      style: AriesTextStyles.textHeading6.copyWith(
+                        color: isSelectedOption
+                            ? AriesColor.yellowP300
+                            : AriesColor.black,
+                        fontWeight: isSelectedOption
+                            ? FontWeight.bold
+                            : FontWeight.w400,
+                      ),
+                    ),
+                  ]),
+                );
+              },
+            ).toList(),
+            onChanged: (newValue) {},
+            value: selectedFrequency,
+            underline: const SizedBox.shrink(),
+            borderRadius: BorderRadius.circular(
+              8.r,
+            ),
+            dropdownColor: AriesColor.neutral0,
+            isExpanded: true,
+          ),
+        )
+      ],
+    );
+  }
+}
