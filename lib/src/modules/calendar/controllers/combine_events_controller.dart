5f837849a78bd597113077fe1e06943e2b96a1fb
Tai-DucTran <taitran.deve@gmail.com>
2024-11-03 22:55:34 +0700

    Calendar: created combine_events_controller

---
+++ lib/src/modules/calendar/controllers/combine_events_controller.dart
@@ -0,0 +1,48 @@
+import 'package:religion_calendar_app/src/modules/calendar/controllers/controllers.dart';
+import 'package:religion_calendar_app/src/modules/calendar/models/models.dart';
+import 'package:religion_calendar_app/src/modules/geoip_and_locales/controllers/controllers.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'combine_events_controller.g.dart';
+
+@riverpod
+class CombineEventsController extends _$CombineEventsController {
+  @override
+  Future<List<BasedEvent>> build() async {
+    final userEventsAsync = ref.watch(userEventControllerProvider);
+    final religionEventsAsync = ref.watch(religionEventControllerProvider);
+    final currentLocale = ref.watch(localeControllerProvider);
+
+    // Wait for both AsyncValues to resolve
+    final List<UserEvent> userEvents = await userEventsAsync.when(
+      data: (data) => data,
+      error: (_, __) => [],
+      loading: () => [],
+    );
+    final convertedUserEventsToBasedEvents = userEvents.map(
+      (event) => BasedEvent.fromUserEvent(event),
+    );
+
+    final List<ReligionEvent> religionEvents = await religionEventsAsync.when(
+      data: (data) => data,
+      error: (_, __) => [],
+      loading: () => [],
+    );
+    final convertedReligionEventsToBasedEvents = religionEvents.map(
+      (event) => BasedEvent.fromReligionEvent(
+        event,
+        currentLocale.languageCode,
+      ),
+    );
+
+    final now = DateTime.now();
+
+    final combinedEvents = [
+      ...convertedUserEventsToBasedEvents.map((e) => e),
+      ...convertedReligionEventsToBasedEvents.map((e) => e),
+    ].where((event) => event.startDate.isAfter(now)).toList()
+      ..sort((a, b) => a.startDate.compareTo(b.startDate));
+
+    return combinedEvents;
+  }
+}
---
+++ lib/src/modules/calendar/controllers/combine_events_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'combine_events_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$combineEventsControllerHash() =>
+    r'1a6fb5c46cb20581c699e46cc20f96f68c2fe03b';
+
+/// See also [CombineEventsController].
+@ProviderFor(CombineEventsController)
+final combineEventsControllerProvider = AutoDisposeAsyncNotifierProvider<
+    CombineEventsController, List<BasedEvent>>.internal(
+  CombineEventsController.new,
+  name: r'combineEventsControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$combineEventsControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CombineEventsController = AutoDisposeAsyncNotifier<List<BasedEvent>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
