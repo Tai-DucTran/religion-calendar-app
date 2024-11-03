9f5b9a07f4c2576460683251cfbd9b29f33f6025
Tai-DucTran <taitran.deve@gmail.com>
2024-11-03 22:55:48 +0700

    Home: display ReligionEvents and UserEvents

--- lib/src/modules/home/widgets/organisms/upcoming_events_section.dart
+++ lib/src/modules/home/widgets/organisms/upcoming_events_section.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:religion_calendar_app/constants/constants.dart';
-import 'package:religion_calendar_app/src/modules/calendar/controllers/user_event_controller.dart';
+import 'package:religion_calendar_app/src/modules/calendar/calendar.dart';
 import 'package:religion_calendar_app/src/modules/home/widgets/widgets.dart';
 
 class UpComingEventsSection extends ConsumerWidget {
@@ -11,7 +11,7 @@ class UpComingEventsSection extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userEvents = ref.watch(userEventControllerProvider);
+    final events = ref.watch(combineEventsControllerProvider);
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 24.h),
@@ -20,23 +20,16 @@ class UpComingEventsSection extends ConsumerWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const UpComingEventsHeader(),
-          userEvents.maybeWhen(
+          events.maybeWhen(
             loading: () => const CircularProgressIndicator(),
             error: (error, stacktrace) => Text('Error: $error'),
             orElse: () => const Offstage(),
-            data: (listOfUserEvent) {
-              if (listOfUserEvent.isEmpty) {
+            data: (listOfEvents) {
+              if (listOfEvents.isEmpty) {
                 return const Text('There are no events');
               }
-              final now = DateTime.now();
-              final sortedEvents = listOfUserEvent
-                  .where((event) => event.startDate.isAfter(now))
-                  .toList()
-                ..sort(
-                  (a, b) => a.startDate.compareTo(b.startDate),
-                );
               final displayedEvents =
-                  sortedEvents.take(maxEventsHomePage).toList();
+                  listOfEvents.take(maxEventsHomePage).toList();
 
               return Column(
                 children: [
