88e4b1e8aab23bee5c83b687b2be3cf9378f32ab
s3713564 <taquocthang98@gmail.com>
2024-11-05 21:13:43 +0700

    Housekeeping: Tai work

---
+++ lib/src/modules/home/utils/get_event_image.dart
@@ -0,0 +1,7 @@
+import 'package:religion_calendar_app/src/modules/calendar/models/models.dart';
+
+String getEventImage({
+  required EventCategory eventCategory,
+}) {
+  return '';
+}
--- lib/src/modules/home/widgets/organisms/upcoming_events_section.dart
+++ lib/src/modules/home/widgets/organisms/upcoming_events_section.dart
@@ -1,4 +1,3 @@
-import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -45,7 +44,6 @@ class UpComingEventsSection extends ConsumerWidget {
                         eventTime: eventTime,
                         eventCategory: event.eventCategory,
                         eventLocation: event.location,
-                        eventImageUrl: AriesImages.defaultFamilyEvent,
                       );
                     },
                   ),
