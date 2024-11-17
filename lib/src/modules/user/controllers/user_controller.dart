7f04b200876e371046e9917f144df674c3b15161
Tai-DucTran <taitran.deve@gmail.com>
2024-11-17 10:53:49 +0700

    User: created user_controller

---
+++ lib/src/modules/user/controllers/user_controller.dart
@@ -0,0 +1,58 @@
+import 'package:religion_calendar_app/src/modules/authentication/authentication.dart';
+import 'package:religion_calendar_app/src/modules/user/models/models.dart';
+import 'package:religion_calendar_app/src/modules/user/repositories/repositories.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_controller.g.dart';
+
+@Riverpod(keepAlive: true)
+class UserController extends _$UserController {
+  @override
+  FutureOr<UserState> build() async {
+    final authState = await ref.watch(authStateControllerProvider.future);
+
+    // If already logged in, fetch user data
+    if (authState.isLoggedIn && authState.userId != null) {
+      final user = await fetchUserInfor(authState.userId!);
+      return UserState(
+        isLoading: false,
+        user: user,
+        error: null,
+      );
+    }
+
+    // Set up listener for future auth changes
+    ref.listen<AsyncValue<AuthState>>(
+      authStateControllerProvider,
+      (previous, next) async {
+        next.whenData((auth) async {
+          if (auth.isLoggedIn && auth.userId != null) {
+            final user = await fetchUserInfor(auth.userId!);
+            state = AsyncData(UserState(
+              isLoading: false,
+              user: user,
+              error: null,
+            ));
+          } else {
+            state = AsyncData(UserState.unknow());
+          }
+        });
+      },
+    );
+
+    return UserState.unknow();
+  }
+
+  UserFirestoreRepository get userFireStoreRepo =>
+      ref.read(userFirestoreRepositoryProvider);
+
+  Future<User?> fetchUserInfor(String userId) async {
+    try {
+      state = const AsyncLoading();
+      return await userFireStoreRepo.getUserDetailedInfor(userId);
+    } catch (error, stackTrace) {
+      state = AsyncError(error, stackTrace);
+      return null;
+    }
+  }
+}
---
+++ lib/src/modules/user/controllers/user_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userControllerHash() => r'0b7360e73755348eb5cf1451eae44ba64b590a88';
+
+/// See also [UserController].
+@ProviderFor(UserController)
+final userControllerProvider =
+    AsyncNotifierProvider<UserController, UserState>.internal(
+  UserController.new,
+  name: r'userControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserController = AsyncNotifier<UserState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
