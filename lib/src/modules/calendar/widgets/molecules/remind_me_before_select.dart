ec413ac762f54e7497d47955aa140c3bb4129f49
s3713564 <taquocthang98@gmail.com>
2024-11-23 16:03:59 +0700

    Calendar: Refactor UI for dropdown button

--- lib/src/modules/calendar/widgets/molecules/remind_me_before_select.dart
+++ lib/src/modules/calendar/widgets/molecules/remind_me_before_select.dart
@@ -1,8 +1,7 @@
 import 'package:aries/aries.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:religion_calendar_app/l10n/localized_keys.dart';
 import 'package:religion_calendar_app/src/modules/calendar/models/models.dart';
 
 class RemindMeBeforeSelect extends ConsumerWidget {
@@ -12,48 +11,41 @@ class RemindMeBeforeSelect extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final options = RemindMeBeforeOptions.values.toList();
     const selectedOption = RemindMeBeforeOptions.atTime;
-
+    final TextStyle textStyle = AriesTextStyles.textBodySmall;
+    
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          LocalizedKeys.eventRemindMeBefore,
-          style: AriesTextStyles.textHeading6,
+        const Icon(
+          CupertinoIcons.bell,
+          color: AriesColor.neutral100,
         ),
         Spacing.sp8,
-        IntrinsicWidth(
-          child: DropdownButton<RemindMeBeforeOptions>(
+        Flexible(
+          child: DropdownButtonFormField<RemindMeBeforeOptions>(
+            value: selectedOption,
             items: options.map(
               (option) {
-                final isSelectedOption = option == selectedOption;
                 return DropdownMenuItem(
                   value: option,
                   child: Row(children: [
                     Text(
                       option.localized,
-                      style: AriesTextStyles.textHeading6.copyWith(
-                        color: isSelectedOption
-                            ? AriesColor.yellowP300
-                            : AriesColor.black,
-                        fontWeight: isSelectedOption
-                            ? FontWeight.bold
-                            : FontWeight.w400,
-                      ),
                     ),
                   ]),
                 );
               },
             ).toList(),
             onChanged: (newValue) {},
-            value: selectedOption,
-            underline: const SizedBox.shrink(),
-            borderRadius: BorderRadius.circular(
-              8.r,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
             ),
-            dropdownColor: AriesColor.neutral0,
-            isExpanded: false,
+            dropdownColor:
+                AriesColor.neutral0,
+            style: textStyle.copyWith(
+              color: AriesColor.black,
+            ),
           ),
-        )
+        ),
       ],
     );
   }
--- lib/src/modules/calendar/widgets/molecules/repeated_frequency_select.dart
+++ lib/src/modules/calendar/widgets/molecules/repeated_frequency_select.dart
@@ -11,30 +11,31 @@ class RepeatedFrequencySelect extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final options = RepeatedFrequency.values.toList();
     const selectedFrequency = RepeatedFrequency.doesNotRepeat;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Spacing.sp8,
-        IntrinsicWidth(
+    return IntrinsicWidth(
+      child: Container(
+        decoration: BoxDecoration(
+          color: AriesColor.neutral0,
+          border: Border.all(
+            color: AriesColor.neutral50,
+          ),
+          borderRadius: BorderRadiusDirectional.circular(
+            8.r,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 12.w,
+          vertical: 4.h,
+        ),
+        child: Center(
           child: DropdownButton<RepeatedFrequency>(
             items: options.map(
               (option) {
-                final isSelectedOption = option == selectedFrequency;
                 return DropdownMenuItem(
                   value: option,
-                  child: Row(children: [
-                    Text(
-                      option.localized,
-                      style: AriesTextStyles.textHeading6.copyWith(
-                        color: isSelectedOption
-                            ? AriesColor.yellowP300
-                            : AriesColor.black,
-                        fontWeight: isSelectedOption
-                            ? FontWeight.bold
-                            : FontWeight.w400,
-                      ),
-                    ),
-                  ]),
+                  child: Text(
+                    option.localized,
+                    style: AriesTextStyles.textBodySmall,
+                  ),
                 );
               },
             ).toList(),
@@ -46,9 +47,10 @@ class RepeatedFrequencySelect extends ConsumerWidget {
             ),
             dropdownColor: AriesColor.neutral0,
             isExpanded: true,
+            isDense: true,
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }
