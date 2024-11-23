51e0d29cd251b33947a554b104099432a9c72c66
s3713564 <taquocthang98@gmail.com>
2024-11-23 16:05:08 +0700

    Calendar: Refactor UI for date time picker

--- lib/src/modules/calendar/widgets/molecules/custom_date_time_select.dart
+++ lib/src/modules/calendar/widgets/molecules/custom_date_time_select.dart
@@ -2,9 +2,9 @@ import 'package:aries/aries.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:full_calender/full_calender_extension.dart';
 import 'package:full_calender/models/lunar_date_time.dart';
-import 'package:intl/intl.dart';
 import 'package:religion_calendar_app/l10n/localized_keys.dart';
 import 'package:religion_calendar_app/constants/constants.dart';
 import 'package:religion_calendar_app/src/modules/calendar/calendar.dart';
@@ -22,10 +22,11 @@ class CustomDateTimeSelect extends ConsumerStatefulWidget {
 
   @override
   ConsumerState<CustomDateTimeSelect> createState() =>
-      _CustomDateTimeSelectState();
+      _CustomDateTimeSelectThangState();
 }
 
-class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
+class _CustomDateTimeSelectThangState
+    extends ConsumerState<CustomDateTimeSelect> {
   /// [_debounce], [_pendingUpdate], [_isScrolling], [_scrollSettleTime]
   /// These values helps to resolve the issue
   ///! ERROR:flutter/impeller/base/validation.cc(59)] Break on 'ImpellerValidationBreak'
@@ -52,15 +53,25 @@ class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
   bool _isScrolling = false;
   static const _scrollSettleTime = Duration(milliseconds: 200);
 
+  late DateTime currentLunarDate;
+
+  late FixedExtentScrollController scrollDayController,
+      scrollMonthController,
+      scrollYearController;
+
   @override
   void initState() {
     super.initState();
     _initializeDateTime();
+    _initialLunarDatetime();
   }
 
   @override
   void dispose() {
     _debounce?.cancel();
+    scrollDayController.dispose();
+    scrollMonthController.dispose();
+    scrollYearController.dispose();
     super.dispose();
   }
 
@@ -70,6 +81,12 @@ class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
     _selectedTime = const TimeOfDay(hour: 08, minute: 08);
   }
 
+  void _initialLunarDatetime() {
+    scrollDayController = FixedExtentScrollController(initialItem: 0);
+    scrollMonthController = FixedExtentScrollController(initialItem: 0);
+    scrollYearController = FixedExtentScrollController(initialItem: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentLocale = Localizations.localeOf(context).toString();
@@ -77,59 +94,58 @@ class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
         ref.watch(calendarCategoryControllerProvider) == CalendarCategory.lunar;
     final isAllDay = ref.watch(isAllDayToggleControllerProvider);
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        _buildLeadingLabel(),
-        Spacing.sp4,
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            _buildDateButton(
-              context,
-              currentLocale,
+            SizedBox(
+              width: 22.w,
             ),
-            _buildDateSubtitle(
-              currentLocale,
-              isLunarCalendar,
-            )
+            Spacing.sp8,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildDateButton(
+                  context,
+                  currentLocale,
+                  isLunarCalendar,
+                ),
+                _buildDateSubtitle(
+                  currentLocale,
+                  isLunarCalendar,
+                ),
+              ],
+            ),
+            const Spacer(),
+            if (!isAllDay) _buildTimeButton(context, currentLocale),
           ],
         ),
-        Spacing.sp16,
-        if (!isAllDay) _buildTimeButton(context, currentLocale),
       ],
     );
   }
 
-  Widget _buildLeadingLabel() {
-    // A work arround to ensure those component having same padding.
+  Widget _buildDateButton(
+      BuildContext context, String locale, bool isLunarCalendar) {
+    const dateFormat = DateTimeFormat.dateMonthYear;
     return CupertinoButton(
       padding: EdgeInsets.zero,
-      alignment: Alignment.center,
-      onPressed: () {},
+      onPressed: () => isLunarCalendar
+          ? _showLunarDatePicker(context)
+          : _showSolarDatePicker(context),
       child: Text(
-        widget.isStartDate ? LocalizedKeys.fromText : LocalizedKeys.toText,
-        style: AriesTextStyles.textHeading6.copyWith(
-          color: AriesColor.black,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDateButton(BuildContext context, String locale) {
-    return CupertinoButton(
-      padding: EdgeInsets.zero,
-      alignment: Alignment.center,
-      onPressed: () => _showDatePicker(context),
-      child: Text(
-        DateFormat(
-          DateTimeFormat.dateMonthYear,
-          locale,
-        ).format(
-          _selectedDate,
-        ),
-        style: AriesTextStyles.textHeading6.copyWith(
+        isLunarCalendar
+            ? getFullLunarDateText(
+                locale: locale,
+                inputDate: _selectedDate,
+                dateFormat: dateFormat,
+              )
+            : getFullSolarDateText(
+                locale: locale,
+                inputDate: _selectedDate,
+                dateFormat: dateFormat,
+              ),
+        style: AriesTextStyles.textBodyNormal.copyWith(
           color: AriesColor.yellowP300,
         ),
       ),
@@ -148,11 +164,10 @@ class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
         day: _selectedDate.day);
     final solarDateFromLunar =
         FullCalenderExtension.convertLunarDateToSolarDate(lunarDate)!;
-
     final subDateLabel = isLunarCalendar
         ? getFullSolarDateText(
             locale: locale,
-            inputDate: solarDateFromLunar,
+            inputDate: _selectedDate,
             dateFormat: dateFormat,
           )
         : getFullLunarDateText(
@@ -173,12 +188,220 @@ class _CustomDateTimeSelectState extends ConsumerState<CustomDateTimeSelect> {
       onPressed: () => _showTimePicker(context),
       child: Text(
         _selectedTime.format(context),
-        style: AriesTextStyles.textHeading6,
+        style: AriesTextStyles.textBodySmall,
       ),
     );
   }
 
-  void _showDatePicker(BuildContext context) {
+  void _showLunarDatePicker(BuildContext context) {
+    late int selectedYear, selectedMonth, selectedDay;
+    late bool isLeap;
+    late int timeZone;
+
+    LunarDateTime currentLunarDate =
+        convertSolarDateToLunarDate(inputDate: _selectedDate);
+    selectedDay = currentLunarDate.day;
+    selectedMonth = currentLunarDate.month;
+    selectedYear = currentLunarDate.year;
+    isLeap = currentLunarDate.isLeap;
+    timeZone = currentLunarDate.timeZone;
+
+    final List<LunarMonth> listAllLunarDaysOfYear =
+        getNumberOfDaysInLunarMonths(selectedYear);
+
+    scrollDayController =
+        FixedExtentScrollController(initialItem: selectedDay - 1);
+
+    for (int i = 0; i < listAllLunarDaysOfYear.length; i++) {
+      if (selectedMonth == listAllLunarDaysOfYear[i].month &&
+          isLeap == listAllLunarDaysOfYear[i].isLeap) {
+        scrollMonthController = FixedExtentScrollController(initialItem: i);
+      }
+    }
+
+    scrollYearController =
+        FixedExtentScrollController(initialItem: selectedYear);
+
+    List<int> listDays = getDaysInMonth(selectedMonth, selectedYear);
+
+    List<String> listMonths = getMonthsInYear(selectedYear);
+
+    List<int> listYears = List.generate(100, (index) => 2000 + index);
+
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setModalState) {
+            return Container(
+              height: 250,
+              color: AriesColor.neutral0,
+              child: NotificationListener<ScrollNotification>(
+                onNotification: _handleScrollNotification,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 60.w,
+                      height: 250,
+                      child: CupertinoPicker(
+                        scrollController: scrollDayController,
+                        looping: true,
+                        itemExtent: 40,
+                        onSelectedItemChanged: (index) {
+                          setModalState(() {
+                            selectedDay = listDays[index];
+                            final newSelectedDate = FullCalenderExtension
+                                .convertLunarDateToSolarDate(LunarDateTime(
+                                    year: selectedYear,
+                                    month: selectedMonth,
+                                    day: selectedDay,
+                                    isLeap: isLeap,
+                                    timeZone: timeZone))!;
+                            setState(() {
+                              _selectedDate = newSelectedDate;
+                            });
+                          });
+                        },
+                        children: listDays
+                            .map(
+                              (day) => Center(
+                                child: Text(
+                                  day.toString(),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 120.w,
+                      height: 250,
+                      child: CupertinoPicker(
+                        scrollController: scrollMonthController,
+                        looping: true,
+                        itemExtent: 40,
+                        onSelectedItemChanged: (index) {
+                          setModalState(() {
+                            final getMonth = listMonths[index];
+                            isLeap = getMonth.endsWith('+');
+                            selectedMonth =
+                                int.parse(getMonth.replaceAll('+', ''));
+                            listDays =
+                                getDaysInMonth(selectedMonth, selectedYear);
+                            for (var all in listAllLunarDaysOfYear) {
+                              if (all.month == selectedMonth &&
+                                  selectedDay > all.days) {
+                                selectedDay--;
+                              }
+                            }
+                            final newSelectedDate = FullCalenderExtension
+                                .convertLunarDateToSolarDate(LunarDateTime(
+                                    year: selectedYear,
+                                    month: selectedMonth,
+                                    day: selectedDay,
+                                    isLeap: isLeap,
+                                    timeZone: timeZone))!;
+
+                            scrollDayController.jumpToItem(selectedDay - 1);
+                            setState(() {
+                              _selectedDate = newSelectedDate;
+                            });
+                          });
+                        },
+                        children: listMonths
+                            .map(
+                              (month) => Center(
+                                child: Text('tháng $month'),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.w,
+                      height: 250,
+                      child: CupertinoPicker(
+                        scrollController: scrollYearController,
+                        looping: true,
+                        itemExtent: 40,
+                        onSelectedItemChanged: (index) {
+                          setModalState(() {
+                            selectedYear = listYears[index];
+                            listDays =
+                                getDaysInMonth(selectedMonth, selectedYear);
+                            listMonths = getMonthsInYear(selectedYear);
+                            final newSelectedDate = FullCalenderExtension
+                                .convertLunarDateToSolarDate(LunarDateTime(
+                                    year: selectedYear,
+                                    month: selectedMonth,
+                                    day: selectedDay,
+                                    isLeap: isLeap,
+                                    timeZone: timeZone))!;
+                            final List<LunarMonth> listAllLunarDaysOfYear =
+                                getNumberOfDaysInLunarMonths(selectedYear);
+                            for (int i = 0;
+                                i < listAllLunarDaysOfYear.length;
+                                i++) {
+                              if (convertSolarDateToLunarDate(
+                                              inputDate: newSelectedDate)
+                                          .month ==
+                                      listAllLunarDaysOfYear[i].month &&
+                                  convertSolarDateToLunarDate(
+                                              inputDate: newSelectedDate)
+                                          .isLeap ==
+                                      listAllLunarDaysOfYear[i].isLeap) {
+                                scrollMonthController.jumpToItem(i);
+                              }
+                            }
+                            scrollDayController.jumpToItem(selectedDay - 1);
+                            setState(() {
+                              _selectedDate = newSelectedDate;
+                            });
+                          });
+                        },
+                        children: listYears
+                            .map(
+                              (year) => Center(
+                                child: Text(
+                                  year.toString(),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  List<String> getMonthsInYear(int year) {
+    final listAllLunarDaysOfYear = getNumberOfDaysInLunarMonths(year);
+    List<String> listMonths = [];
+    for (var all in listAllLunarDaysOfYear) {
+      listMonths.add('${all.month}${all.isLeap ? '+' : ''}');
+    }
+    return listMonths;
+  }
+
+  List<int> getDaysInMonth(int month, int year) {
+    final listAllLunarDaysOfYear = getNumberOfDaysInLunarMonths(year);
+    int days = 0;
+    for (var all in listAllLunarDaysOfYear) {
+      if (month == all.month) {
+        days = all.days;
+      }
+    }
+    return List.generate(days, (index) => 1 + index);
+  }
+
+  void _showSolarDatePicker(BuildContext context) {
     showCupertinoModalPopup(
       context: context,
       builder: (BuildContext context) {
