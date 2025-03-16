42917010df4ee32e410e3bec6a2b0e9faf51aecd
Tai-DucTran <taitran.deve@gmail.com>
2025-03-16 21:23:36 +0700

    Housekeeping: temp created a new onboarding_v2 page

--- lib/src/modules/onboarding/widgets/page/onboarding_v2.dart
+++ lib/src/modules/onboarding/widgets/page/onboarding_v2.dart
@@ -1,5 +1,6 @@
 import 'package:aries/aries.dart';
 import 'package:flutter/material.dart';
+import 'package:religion_calendar_app/src/utils/localization_extension.dart';
 
 class OnboardingPageV2 extends StatelessWidget {
   const OnboardingPageV2({
@@ -12,20 +13,32 @@ class OnboardingPageV2 extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       decoration: BoxDecoration(
-        gradient: LinearGradient(
+        gradient: const LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            Colors.white,
+            AriesColor.yellowP50,
             AriesColor.yellowP100,
-            AriesColor.yellowP100,
+            AriesColor.yellowP200,
           ],
         ),
         borderRadius: BorderRadius.circular(24.0),
       ),
       padding: const EdgeInsets.all(16.0),
       child: Column(
-        children: [const Text('Tôn giáo của bạn là')],
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 128, left: 42, right: 42),
+            child: Text(
+              context.l10n.myReligionIs,
+              style: AriesTextStyles.textHeading3.copyWith(
+                fontSize: 26,
+                decoration: TextDecoration.none,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
     );
   }
